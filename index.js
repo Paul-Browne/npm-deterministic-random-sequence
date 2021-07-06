be80fc66f6167c0937155c8ca9be1ef4240315a0
paul browne <paulmatthewbrowne@gmail.com>
2021-07-06 10:47:54 +0300

    fixes whitespace

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
 const xmur3 = (str) => {
-    for(var i = 0, h = 1779033703 ^ str.length; i < str.length; i++)
-        h = Math.imul(h ^ str.charCodeAt(i), 3432918353),
-        h = h << 13 | h >>> 19;
-    return function() {
-        h = Math.imul(h ^ h >>> 16, 2246822507);
-        h = Math.imul(h ^ h >>> 13, 3266489909);
-        return (h ^= h >>> 16) >>> 0;
-    }
+  for(var i = 0, h = 1779033703 ^ str.length; i < str.length; i++)
+    h = Math.imul(h ^ str.charCodeAt(i), 3432918353),
+    h = h << 13 | h >>> 19;
+  return function() {
+    h = Math.imul(h ^ h >>> 16, 2246822507);
+    h = Math.imul(h ^ h >>> 13, 3266489909);
+    return (h ^= h >>> 16) >>> 0;
+  }
 }
 
 const sfc32 = (a, b, c, d) => {
-    return function() {
-      a |= 0; b |= 0; c |= 0; d |= 0; 
-      var t = (a + b | 0) + d | 0;
-      d = d + 1 | 0;
-      a = b ^ b >>> 9;
-      b = c + (c << 3) | 0;
-      c = c << 21 | c >>> 11;
-      c = c + t | 0;
-      return (t >>> 0) / 4294967296;
-    }
+  return function() {
+    a |= 0; b |= 0; c |= 0; d |= 0; 
+    var t = (a + b | 0) + d | 0;
+    d = d + 1 | 0;
+    a = b ^ b >>> 9;
+    b = c + (c << 3) | 0;
+    c = c << 21 | c >>> 11;
+    c = c + t | 0;
+    return (t >>> 0) / 4294967296;
+  }
 }
 
 module.exports = arr => {
-	var str = arr ? arr.toString() : "";
-	var seed = xmur3(str);
-	var rand = sfc32(seed(),seed(),seed(),seed());
-	return rand;
+  var str = arr ? arr.toString() : "";
+  var seed = xmur3(str);
+  var rand = sfc32(seed(),seed(),seed(),seed());
+  return rand;
 }
